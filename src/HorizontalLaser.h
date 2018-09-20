bd12d6d7cf3fe6112e96ccdc0ebe4667e6b00903
Vitor Leal <vitinho2201@gmail.com>
2018-09-20 18:44:53 -0300

    Incluindo comentarios na classe horizontalLaser
    Signed-off-by: Vitor Leal <vitinho2201@gmail.com>

--- src/HorizontalLaser.h
+++ src/HorizontalLaser.h
@@ -1,17 +1,17 @@
-/*
- * HorizontalLaser.h
- *
- *  Created on: 12/07/2015
- *      Author: Vitor
- *  Reviewed on: 02/09/2018
- * 		Reviewer: Vitor
- */
+/*********************************************************
+  * File: CutScene1.cpp
+  * Purpose: Implementation of laser shooted by the enemies
+*********************************************************/
 
 #ifndef SRC_HorizontalLaser_H_
 #define SRC_HorizontalLaser_H_
 
-#include "GameObject.h"
-#include "Timer.h"
+#include "GameObject.h" /* Class header file to include horizontalLaser 
+						   as an object 
+						*/
+#include "Timer.h" /* Classe header file necessary to calculate time for 
+					  updating object position
+					*/
 
 class HorizontalLaser : public GameObject{
 public:
@@ -25,8 +25,8 @@ public:
 	void NotifyCollision(GameObject&);
 	bool Is(string type);
 private:
-	Sprite sp;
-	GameObject* planet;
+	Sprite sp; /* Laser sprite */
+	GameObject* planet; /* Object where the laser will be included */
 	float initialHeight;
 	Timer timer;
 	int loadedFrame;

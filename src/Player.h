a0b21481a3bcf52ecd6f2637738cc6224e328426
Gabriela Barrozo Guedes <gabrielabguedes@gmail.com>
2018-09-21 11:27:09 -0300

    Arrumando alguns cometários
    Signed-off-by: Gabriela Barrozo Guedes <gabrielabguedes@gmail.com>

--- src/Player.h
+++ src/Player.h
@@ -13,6 +13,7 @@
 #include "EndState.h"
 #include "StateData.h"
 
+//Enumerates the jumping states of the player
 enum Jumping { STAND, JUMP, DJUMP };
 
 /*
@@ -45,19 +46,19 @@ public:
 	Timer deathAnimation;
 
 private:
-	Sprite body;          /* Player's character                                */
-	Point speed;          /* The speed it walks                                */
-	int hp;               /* Health points                                     */
-	int xp;               /* Experience points                                 */
-	Timer dmgCD;          /* Damage time??                                     */
-	Timer knockback;      /* Timer to count the time the player is pushed back */
-	Jumping jumpState;    /* The position and state the jump of the player is  */
-	int jumpY;            /* The height of the jump                            */
-	int jumped;           /* ??????????                                        */
-	Facing kbDirection;   /* ??????????                                        */
-	int c;                /* ??????????                                        */
-	int qntEnergia;       /* Amount of energy                                  */
-	Timer energiaCD;      /* ??????????                                        */
+	Sprite body;          // Player's characte
+	Point speed;          // The speed it walk
+	int hp;               // Health points
+	int xp;               // Experience points                                 
+	Timer dmgCD;          // "Damage cool down" time between demages                                     
+	Timer knockback;      // Timer to count the time the player is pushed back 
+	Jumping jumpState;    // The position and state the jump of the player is  
+	int jumpY;            // The height of the jump                            
+	int jumped;           // How much the player has jumped                                        
+	Facing kbDirection;   // ???                    
+	int c;                // center -----> REFATORAR
+	int qntEnergia;       // Amount of energy -------> REFATORAR           
+	Timer energiaCD;      // Time between energies ----->REFATORAR
 
 };
 

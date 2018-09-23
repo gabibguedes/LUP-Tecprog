7bce7cd1e0beec8ed3f922c1e02370728d110730
Gabriel Alves <gabrielsoares.prog@gmail.com>
2018-09-23 12:05:25 -0300

    Add class Enemy3 (cpp & h)

--- src/Enemy3.cpp
+++ src/Enemy3.cpp
@@ -1,3 +1,8 @@
+/******************************************
+ * File: 	Enemy3.cpp
+ * Purpose: Implementation Enemy3 class 
+ *****************************************/
+
 #include "Enemy3.h"
 
 #include <cmath>
@@ -8,18 +13,35 @@
 #include "Game.h"
 #include "State.h"
 
-Enemy3::Enemy3(float x, float y, GameObject* planet, float initialRotation, float alturaInicial) : sp("img/enemy3.png", 0.375, 3, 7), speed(), startPos(x, y), dmgCD(), knockback(), shootcd()
-{
+/**
+ * Constructor of Enemy3.
+ * 
+ * @param x, initial value of the location of the object on the x-axis (type real)
+ * @param y, initial value of the location of the object on the y-axis (type real)
+ * @param planet, pointer to GameObject
+ * @param initialRotation, initial value of object rotation (type real)
+ * @param initialHeight, initial height value of the object (type real)
+ * 
+ */
+
+Enemy3::Enemy3(float x, float y, GameObject* planet, float initialRotation,
+float initialHeight) : 
+	sp("img/enemy3.png", 0.375, 3, 7), speed(), startPos(x, y),
+	dmgCD(), knockback(), shootcd() {
 	this->planet = planet;
 	sp.SetScaleX(0.5);
 	sp.SetScaleY(0.5);
 	box.setH(sp.GetHeight());
 	box.setW(sp.GetWidth());
 	rotation = initialRotation;
-	float arc = rotation*3.1415/180;
-	this->alturaInicial = alturaInicial;
-	box.setX(planet->box.getCenterX() + ((planet->box.getW()/2 + planet->box.getCenterY() + alturaInicial+box.getH())*cos(arc)) - (box.getW()/2));
-	box.setY(planet->box.getCenterY()  + ((planet->box.getH()/2 + planet->box.getCenterY()  + alturaInicial+box.getH())*sin(arc)) - (box.getH()/2));
+	float arc = rotation*3.1415/180; // converts from degrees to radians
+	initialHeight = alturaInicial;
+	box.setX(planet->box.getCenterX() + ((planet->box.getW()/2 +
+	planet->box.getCenterY() + initialHeight+box.getH())*cos(arc)) -
+	(box.getW()/2));
+	box.setY(planet->box.getCenterY()  + ((planet->box.getH()/2 +
+	planet->box.getCenterY()  + initialHeight+box.getH())*sin(arc)) -
+	(box.getH()/2));
 	hp = 50;
 	speed.x = 2;
 	speed.y = 0;
@@ -33,21 +55,26 @@ Enemy3::~Enemy3()
 
 }
 
-void Enemy3::Update(float dt)
-{
+/**
+ * @brief used to update the position of the object in relation to the time
+ * @param deltaTime: elapsed time since last update
+*/
+void Enemy3::Update(float deltaTime) {
 	Point* playerPos = new Point(Player::player->box.getCenterX(),
 			Player::player->box.getCenterY());
 	Point* currentPos = new Point(box.getCenterX(), box.getCenterY());
+	const  PI double = 3.1415; // constant of PI
+	const  AUX_CONVERSION int = 180; //constant that assists in the conversion to radian
 
-	dmgCD.Update(dt);
-	if (knockback.Get() < 0.3)
-	{
+	dmgCD.Update(deltaTime);
+
+	if (knockback.Get() < 0.3) { 
 		if (kbDirection == LEFT)
-			rotation -= 18*dt;
+			rotation -= 18*deltaTime;
 		else
-			rotation += 18*dt;
+			rotation += 18*deltaTime;
 
-		sp.Update(dt);
+		sp.Update(deltaTime);
 	}
 	else if (currentPos->getDist(*playerPos) <= 700
 			&& currentPos->getDist(*playerPos) >= 500) {
@@ -55,17 +82,17 @@ void Enemy3::Update(float dt)
 		if (playerPos->x > currentPos->x) {
 			orientation = RIGHT;
 			sp.SetFlipH(true);
-			rotation += 5*dt;
+			rotation += 5*deltaTime;
 		} else if (playerPos->x < currentPos->x) {
 			orientation = LEFT;
 			sp.SetFlipH(false);
-			rotation -= 5*dt;
+			rotation -= 5*deltaTime;
 		}
-		sp.Update(dt);
+		sp.Update(deltaTime);
 	} else if(currentPos->getDist(*playerPos) <= 500) {
 		sp.SetLoop(0, 13);
-		if ((sp.GetCurrentFrame() == 4 || sp.GetCurrentFrame() == 11) && shootcd.Get() > 0.35)
-		{
+		if ((sp.GetCurrentFrame() == 4 || sp.GetCurrentFrame() == 11) &&
+			shootcd.Get() > 0.35) {
 			if (playerPos->x > currentPos->x) {
 				orientation = RIGHT;
 				sp.SetFlipH(true);
@@ -76,7 +103,7 @@ void Enemy3::Update(float dt)
 			Shoot(*playerPos);
 			shootcd.Restart();
 		}
-		sp.Update(dt);
+		sp.Update(deltaTime);
 	} else {
 		sp.SetFrame(0);
 		sp.Update(1);
@@ -92,47 +119,78 @@ void Enemy3::Update(float dt)
 	somaRotation = planet->somaRotation;
 	rotation += somaRotation;
 
-	float arc = rotation*3.1415/180;
-	box.setX(planet->box.getCenterX() + ((planet->box.getW()/2 - 300 + alturaInicial+box.getH())*cos(arc)) - (box.getW()/2));
-	box.setY(planet->box.getCenterY()  + ((planet->box.getH()/2 - 300 + alturaInicial+box.getH())*sin(arc)) - (box.getH()/2));
+	float arc = rotation*PI/HALF_TURN; //converts from degrees to radians
 
-	knockback.Update(dt);
-	shootcd.Update(dt);
+	// Set new position of Box
+	box.setX(planet->box.getCenterX() + ((planet->box.getW()/2 - 300 +
+	initialHeight+box.getH())*cos(arc)) - (box.getW()/2));
+	box.setY(planet->box.getCenterY()  + ((planet->box.getH()/2 - 300 +
+	initialHeight+box.getH())*sin(arc)) - (box.getH()/2));
+
+	knockback.Update(deltaTime);
+	shootcd.Update(deltaTime);
 }
 
+/**
+ * @brief used to render the object vertically, so that it does not
+ * overlap on another object.
+*/
 void Enemy3::Render()
 {
-	sp.Render(box.getX() + Camera::pos.getX(), box.getY() + Camera::pos.getY(),rotation + 90);
+	const MOVE_UP int = 90;
+	sp.Render(box.getX() + Camera::pos.getX(), box.getY() + Camera::pos.getY(),
+	rotation + MOVE_UP);
 }
 
+/**
+ * @brief used to check if the object is dead
+ * @return bool: true or false
+*/
 bool Enemy3::IsDead()
 {
 	return (hp <= 0);
 }
+/**
+ * @brief method get standard, for the Sprite
+ * @return Sprite
+ */
 
 Sprite Enemy3::getSprite()
 {
 	return sp;
 }
 
+/**
+ * @brief used to check if the object is an Enemy3
+ * @return true or false
+ */
 bool Enemy3::Is(string type)
 {
 	return (type == "Enemy3");
 }
 
+/**
+ * @brief used for class object to shoot
+ * @param pos, starting shot position
+ */
 void Enemy3::Shoot (Point pos)
 {
-	Point* pspeed = new Point(box.getCenterX() - pos.getX(),box.getCenterY() - pos.getY());
-	//float speed = pspeed->magVector();
+	const  PI double = 3.141592653; // Constatnt PI
+	Point* pspeed = new Point(box.getCenterX() - pos.getX(),box.getCenterY() -
+	pos.getY());
 	float angle = atan(pspeed->getY()/pspeed->getX());
 	if(pspeed->getX() > 0){
-		angle += 3.141592653;
+		angle += PI;
 	}
 	delete(pspeed);
 
 	float xBullet;
 	float yBullet;
 
+	/**
+	 * @brief Direct the shot correctly
+	 * result: shot direction
+	*/
 	if(orientation == LEFT)
 	{
 		if (sp.GetCurrentFrame() == 4)
@@ -160,13 +218,19 @@ void Enemy3::Shoot (Point pos)
 		}
 	}
 
-	Bullet* bullet = new Bullet(xBullet, yBullet, planet, angle, 0, 500, 2000,"img/enemy3_bullet.png", true, 4);
+	Bullet* bullet = new Bullet(xBullet, yBullet, planet, angle, 0, 500,
+	2000,"img/enemy3_bullet.png", true, 4); //Identação
 	Game::GetInstance().GetCurrentState().AddObject(bullet);
 }
 
-void Enemy3::NotifyCollision(GameObject& other)
-{
-	if ((other.Is("WeaponBroom") && other.attacking) || (other.Is("WeaponSword") && other.attacking) || (other.Is("Support") && other.attacking)) {
+/**
+ * @brief used to notify character status after an attack
+ * @param, a type of "physical" object of the game, such as "WeaponBroom"
+*/
+void Enemy3::NotifyCollision(GameObject& other) {
+	if ((other.Is("WeaponBroom") && other.attacking) ||
+		(other.Is("WeaponSword") && other.attacking) ||
+		(other.Is("Support") && other.attacking)) {
 
 		if (dmgCD.Get() > 0.5) {
 			Sound* sound = new Sound("audio/dano.wav");
--- src/Enemy3.h
+++ src/Enemy3.h
@@ -1,9 +1,8 @@
-/*
- * Enemy3.h
- *
- *  Created on: 23 Jun 2015
- *      Author: Baile
- */
+/******************************************
+ * File: Enemy3.h
+ * Purpose: this class represents a
+ * character (enemy) of the game
+ *****************************************/
 
 #ifndef SRC_ENEMY3_H_
 #define SRC_ENEMY3_H_
@@ -28,17 +27,17 @@ public:
 	void Shoot (Point pos);
 	void NotifyCollision(GameObject&);
 private:
-	Sprite sp;
-	Point speed;
-	Point startPos;
-	int hp;
-	Timer dmgCD;
-	Facing orientation;
-	GameObject* planet;
-	float alturaInicial;
-	Timer knockback;
-	Timer shootcd;
-	Facing kbDirection;
+	Sprite sp; // Sprite to show the frames of the story
+	Point speed; // Reference point for speed calculation
+	Point startPos; // References point to set start position
+	int hp; // saves the amount life of the character
+	Timer dmgCD; // Time damage
+	Facing orientation; // Enum of orientation of object
+	GameObject* planet; // Pointer for planet (a type of game object)
+	float alturaInicial; // Initial Height of character
+	Timer knockback; // Time of knockback
+	Timer shootcd; // Time of shoot
+	Facing kbDirection; // Enum of Direction of object
 };
 
 

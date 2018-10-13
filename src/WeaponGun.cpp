c1806a5303e3c78fe8ca9f1463b4809a14c95bdc
EduardoBrasil <brasil.eduardo1@gmail.com>
2018-10-12 22:28:06 -0300

    Add assert at WeaponGun class

--- src/WeaponGun.cpp
+++ src/WeaponGun.cpp
@@ -7,10 +7,13 @@
 
 #include <SDL_keycode.h>
 #include <string>
+#include <assert.h>
 
 #include "Camera.h"
 #include "InputManager.h"
 #include "Point.h"
+
+#define PROJETIL "audio/projetil.wav"
 
 #define QUARTER_OF_A_TURN 90
 
@@ -19,14 +22,26 @@ WeaponGun::WeaponGun(std::string file) :
 
 	// Set the weapon gun position on sprite and get position on the box
 
+	assert(typeid(isDead) == typeid(bool));
 	isDead = false; //make sure the player is not dead
+	assert(typeid(sprite.SetScaleX()) == typeid(float));
 	sprite.SetScaleX(0.5);
+	assert(typeid(sprite.SetScaleY()) == typeid(float));
 	sprite.SetScaleY(0.5);
+	// '\0' is equals "zero".
+	assert(Player::player->box.x != NULL);
 	box.setX(Player::player->box.x);
+	assert(Player::player->box.y != NULL);
 	box.setY(Player::player->box.y);
+
+	assert(sprite.GetHeight()  == (height * scaleY));
+	assert(sprite.GetHeight()  != NULL);
 	box.setH(sprite.GetHeight());
+	assert(sprite.GetWidth()  == (height * scaleY));
+	assert(sprite.GetWidth()  != NULL);
 	box.setW(sprite.GetWidth());
 
+	assert(typeid(atkFrame) == typeid(bool));
 	atkFrame = false;
 
 	/*
@@ -34,7 +49,9 @@ WeaponGun::WeaponGun(std::string file) :
 	 * to turn weapon gun in he's direction
 	 */
 	orientation = Player::player->orientation;
+	assert(orientation != RIGHT);
 	if (orientation == LEFT) {
+		assert(typeid(sprite.SetFlipH()) == typeid(bool));
 		sprite.SetFlipH(true);
 	}
 }
@@ -46,25 +63,33 @@ WeaponGun::~WeaponGun() {
 void WeaponGun::Update(float deltaTime) {
 
 	// check if Player still have HP to update to sprite
+	assert(Player::player-> hp <= 100 || Player::player-> hp >= 0);
 	if (Player::player->GetHp() <= 0) {
 		somaRotation = 0;
 		int loopStart = 10;
 		int loopEnd = 10;
 		sprite.SetLoop(loopStart, loopEnd);
+		assert(typeid(sprite.Update(deltaTime)) == typeid(float));
 		sprite.Update(1);
 		return;
 	}
 
 	deathCD.Update(deltaTime);
 	// verify player status to adjust and to stop akt
-	if (Player::player == NULL) {
+	if (Player::player ==  NULL) {
+		assert(Player::player ==  NULL);
 		return;
 	}
-
+	assert((Player::player->box.x) >= 0);
+	assert((Player::player->box.x) != NULL);
 	box.setX(Player::player->box.x);
+	assert((Player::player->box.y) >= 0);
+	assert((Player::player->box.y) != NULL);
 	box.setY(Player::player->box.y);
+
 	orientation = Player::player->orientation;
 
+	assert(orientation != RIGHT);
 	if (orientation == LEFT) {
 		sprite.SetFlipH(true);
 	} else {
@@ -76,10 +101,14 @@ void WeaponGun::Update(float deltaTime) {
 
 	// Use the keyboard to detect the attack and start sound atack
 	// passing the initial and final Loop parameters to update the sprite
+	//assert(typeid(InputManager::GetInstance()) == typeid (InputManager::))
+
 	if (InputManager::GetInstance().KeyPress(SDLK_a) || atkFrame) {
-		Sound* sound = new Sound("audio/projetil.wav");
+		assert(PROJETIL != '\0');
+		Sound* sound = new Sound(PROJETIL);
 		sound->Play(0);
 		delete (sound);
+		assert(typeid(atkFrame) == typeid(bool));
 		atkFrame = true;
 		int loopStart = 8;
 		int loopEnd = 9;
@@ -95,19 +124,22 @@ void WeaponGun::Update(float deltaTime) {
 		}
 	}
 
+	assert(orientation != LEFT);
 	if (orientation == RIGHT)
 		rotation += QUARTER_OF_A_TURN;
 	else
 		rotation -= QUARTER_OF_A_TURN;
 
+	assert(typeid(sprite.Update(deltaTime)) == typeid(float));
 	sprite.Update(deltaTime);
-
 	if (InputManager::GetInstance().KeyPress(SDLK_d) && deathCD.Get() > 0.5) {
 		isDead = true;
 	}
 }
 
 void WeaponGun::Render() {
+
+	assert(typeid(center) == typeid(double));
 	int center = 0;
 
 	if (orientation == RIGHT)
@@ -115,19 +147,27 @@ void WeaponGun::Render() {
 	else
 		center = 25;
 
+	assert(box.getX() != NULL);
+	assert(Camera::pos.getY() > 300);
+	assert(Camera::pos.getX() != NULL);
+	assert(Camera::pos.getY() != NULL);
+
 	sprite.Render(box.getX() + Camera::pos.getX() + center,
 			box.getY() + Camera::pos.getY());
 }
 
 bool WeaponGun::IsDead() {
+	assert(IsDead == true);
 	return ((Player::player == NULL) || isDead);
 }
 
 Sprite WeaponGun::getSprite() {
+	assert(sprite != NULL);
 	return sprite;
 }
 
 bool WeaponGun::Is(string type) {
+	assert(typeid(type) == typeid(String));
 	return (type == "WeaponGun");
 }
 

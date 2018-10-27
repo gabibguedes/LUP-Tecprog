3b5b74771463f42cd3411ebde9920f9cd635db11
Gabriela Barrozo Guedes <gabrielabguedes@gmail.com>
2018-10-26 23:56:13 -0300

    Refatorando classe Minion
    Signed-off-by: Gabriela Barrozo Guedes <gabrielabguedes@gmail.com>

--- src/Minion.cpp
+++ src/Minion.cpp
@@ -15,10 +15,15 @@ Minion::Minion(GameObject* minionCenter,float arcOffSet) : sprite("img/minion.pn
 	center = minionCenter;
 	arc = arcOffSet;
 	rotation = (arc*HALF_TURN/PI)-HALF_TURN;
+
 	box.setH(sprite.GetHeight());
 	box.setW(sprite.GetWidth());
-	box.setX(center->box.getCenterX() + (200*cos(arc)) - (box.getW()/2));
-	box.setY(center->box.getCenterY()  + (200*sin(arc)) - (box.getH()/2));
+
+  float x = center->box.getCenterX() + (200*cos(arc)) - (box.getW()/2);         
+  float y = center->box.getCenterY()  + (200*sin(arc)) - (box.getH()/2);        
+	box.setX(x);                                                                  
+	box.setY(y);                                                                  
+
 	float scale = (((rand())%50)+100)/100.0;
 	sprite.SetScaleX(scale);
 	sprite.SetScaleY(scale);
@@ -29,14 +34,16 @@ based on the movement pattern and the previous position*/
 void Minion::Update(float deltaTime){
 	arc += PI/10*deltaTime;
 	rotation = (arc*HALF_TURN/PI)-HALF_TURN;
-	float x = center->box.getCenterX() + (100*cos(arc)) - (box.getW()/2);
-	float y = center->box.getCenterY()  + (100*sin(arc)) - (box.getH()/2);
-	box.setX(x);
-	box.setY(y);
+	float x = center->box.getCenterX() + (100*cos(arc)) - (box.getW()/2);         
+	float y = center->box.getCenterY()  + (100*sin(arc)) - (box.getH()/2);        
+	box.setX(x);                                                                  
+	box.setY(y);                                                                  
 }
 
 void Minion::Render(){
-	sprite.Render(box.getX() + Camera::pos.getX(),box.getY() +  Camera::pos.getY(),rotation);
+  float x = box.getX() + Camera::pos.getX();                                    
+  float y = box.getY() +  Camera::pos.getY();                                   
+	sprite.Render(x, y ,rotation);                                                
 }
 
 bool Minion::IsDead(){
@@ -44,11 +51,15 @@ bool Minion::IsDead(){
 }
 
 void Minion::Shoot(Point pos){
-	Point* pspeed = new Point(box.getCenterX() - pos.getX(),box.getCenterY() - pos.getY());
+  float x = box.getCenterX() - pos.getX();
+  float y = box.getCenterY() - pos.getY();
+	Point* pspeed = new Point(x,y);
 	float angle = atan(pspeed->getY()/pspeed->getX());
 	if(pspeed->getX() > 0){
 		angle += PI;
-	}
+	} else {                                                                      
+    // Nothing to do                                                            
+  }
 	delete(pspeed);
 }
 
@@ -61,5 +72,5 @@ bool Minion::Is(string type){
 }
 
 void Minion::NotifyCollision(GameObject& other){
-
+  // Nothing to do                                                              
 }
